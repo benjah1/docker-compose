21dc3065a9c717d86d89225aa41f90c9fdd7c189
Benjah1 <benjaminhuang1@gmail.com>
2017-02-16 16:15:54 +0000

    update redis cluster

--- redis_cluster/entrypoint.sh
+++ redis_cluster/entrypoint.sh
@@ -7,9 +7,7 @@ delay () {
   "${@}";
 }
 
-if [ "$1" = 'slave' ]; then
-	REHOST=$(nslookup tasks.redis localhost | grep Address | cut -d ' ' -f 3 | head -n 2 | tail -n 1)
-	delay 10 redis-trib.rb add-node --slave $(hostname -i):6379 $REHOST:6379 &
-fi
+REHOST=$(nslookup $1 localhost | grep Address | cut -d ' ' -f 3 | head -n 2 | tail -n 1)
+delay 10 redis-trib.rb add-node --slave $(hostname -i):6379 $REHOST:6379 &
 
 redis-server /usr/local/etc/redis/redis.conf
