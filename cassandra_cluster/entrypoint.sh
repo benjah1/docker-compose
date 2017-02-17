d3bf7f26dd945a34bb2110307e0227b7d105da54
Benjah1 <benjaminhuang1@gmail.com>
2017-02-17 16:14:55 +0000

    update cassandra_cluster

--- cassandra_cluster/entrypoint.sh
+++ cassandra_cluster/entrypoint.sh
@@ -2,5 +2,6 @@
 set -e
 
 CASSANDRA_SEEDS=$(dig +short $(echo ${SEED} | tr "," "\n") | paste -sd "," -)
+sed -ri 's/(- seeds:).*/\1 "'"$CASSANDRA_SEEDS"'"/' "$CASSANDRA_CONFIG/cassandra.yaml"
 
 bash -x /docker-entrypoint.sh $@
